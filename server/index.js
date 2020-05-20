b7550c51f741d5c8cfde14c7b33de794372873e8
LeiLei Ros <leilei.ros+dev@gmail.com>
2020-05-20 17:29:37 -0400

    Mini-refactor to add env variables and update README

--- server/index.js
+++ server/index.js
@@ -4,27 +4,33 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const apiItemsURL = process.env.apiItemsURL || 'http://localhost:3001';
+const itemsURL = process.env.itemsURL || 'http://localhost:3002';
+const relatedProdURL = process.env.relatedProdURL || 'http://localhost:3003';
+const reviewsURL = process.env.reviewsURL || 'http://localhost:3004';
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/items/:id', createProxyMiddleware({
-  target: 'http://3.132.5.204:3001/',
+  target: apiItemsURL,
   changeOrigin: true,
 }));
 
 app.use('/items/:id', createProxyMiddleware({
-  target: 'http://34.201.53.74:3002/',
+  target: itemsURL,
   changeOrigin: true,
 }));
 
 app.use('/api/related_products/:id', createProxyMiddleware({
-  target: 'http://54.166.182.193:3003/',
+  target: relatedProdURL,
   changeOrigin: true,
 }));
 
 app.use('/api/allreviews', createProxyMiddleware({
-  target: 'http://18.212.184.37:3004/',
+  target: reviewsURL,
   changeOrigin: true,
 }));
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Proxy server hosted at http://localhost:${port}`);
+});
